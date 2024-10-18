1391a6f1a30c79b3fef234e2323a23654170b443
ladelpriore <lucasdelpriore@gmail.com>
2024-10-18 07:26:52 -0500

    Commit block 16 workshop: groceries.

---
+++ README.md
@@ -0,0 +1,11 @@
+Unit 2 | Grocery Store
+For this workshop, you will be building a simple app that asks for some input data, and will use array methods to manipulate and retrieve data from our array of grocery items.
+
+Instructions
+Create a new repository using this one as a template.
+Clone your repo down and open it in VS Code.
+Live serve index.html. You should be greeted with a prompt.
+Complete the functions described in index.js!
+Submission
+Submit a link to your Github repo.
+
---
+++ index.js
@@ -0,0 +1,147 @@
+/**
+ * @typedef Item
+ * @property {number} id - this item's ID
+ * @property {string} name - name of this item
+ * @property {number} price - price of this item
+ * @property {string} category - the food group this item belongs to
+ * @property {number} quantity - naumber of this item in inventory
+ */
+
+// ------------------ Complete the functions written below ------------------------------ //
+
+                                                /**
+                                                 * Prints out the name of each item in the given array.
+                                                 * @param {Item[]} items - array of items
+                                                 */
+
+
+
+function logNames(items) {
+    items.forEach(item => {
+      console.log(item.name);
+    });
+  }
+
+  
+                                                /**
+                                                 * @param {Item[]} items - array of items
+                                                 * @returns {string[]} an array of item names in all uppercase
+                                                 */
+
+
+
+  function getUppercaseNames(items) {
+    return items.map(item => item.name.toUpperCase());          //Use .map method
+  }                                             //Use toUpperCase on the string name
+                                                // @property {number} price - price of this item
+
+ 
+
+  
+                                                /**
+                                                 * @param {Item[]} items - array of items
+                                                 * @param {number} id - id of the item to find
+                                                 * @returns {Item} - the item in `items` with the given `id`
+                                                 */
+
+  
+
+  function getItemById(items, id) {
+    return items.find(item => item.id === id);  //Use .find array method to return the array item value
+}                                               // @property {number} id - this item's ID
+                                            
+
+  
+                                                /**
+                                                 * @param {Item[]} items - array of items
+                                                 * @param {string} name - name of the item to find
+                                                 * @returns {number} the price of the item named `name`
+                                                 */
+
+
+
+  function getItemPriceByName(items, name) {    //Create a function with parameters items and name
+    for (let item of items) {                   //Nest a for loop in the function 
+      if (item.name === name) {                 //Add an if statement to check that the item name is equal
+        return item.price;                      //If item name matches, return the price
+      }                                         //* @property {number} price - price of this item
+    }
+    return null;                                // Return null if nothing is found
+  }
+
+
+
+  
+                                                /**
+                                                 * @param {Item[]} items - array of items
+                                                 * @param {string} category
+                                                 * @returns {Item[]} array of items that belong to the given `category`
+                                                 */
+
+
+
+  function getItemsByCategory(items, category) {                         //Create a function to organize item array
+    return items.filter(item => item.category === category);             //Use .filter array method to create new array with items grouped in kind
+  }                                                                      // * @property {string} category - the food group this item belongs to
+
+
+  
+                                                /**
+                                                 * @param {Item[]} items - array of items
+                                                 * @returns {number} the total quantity of all items
+                                                 */
+ 
+
+
+  function countItems(items) {
+    return items.reduce((total, item) => total + item.quantity, 0);                   //@property {number} quantity - naumber of this item in inventory
+  }                                                                         //Use reduce() array method to create a single value from the array values
+
+
+
+  /**
+   * @param {Item[]} items - array of items
+   * @returns {number} the cost of all given items
+   */
+  function calculateTotalPrice(items) {
+    // TODO: use `reduce`
+  }
+  
+  // --------------------- DO NOT CHANGE THE CODE BELOW ------------------------ //
+  
+  /** @type {Item[]} */
+  const INVENTORY = [
+    { id: 1, name: "apple", price: 1.75, category: "fruit", quantity: 100 },
+    { id: 2, name: "banana", price: 0.25, category: "fruit", quantity: 137 },
+    { id: 3, name: "orange", price: 1.0, category: "fruit", quantity: 10 },
+    { id: 4, name: "broccoli", price: 3.0, category: "vegetable", quantity: 67 },
+    { id: 6, name: "milk", price: 5.75, category: "dairy", quantity: 90 },
+    { id: 7, name: "cheddar", price: 4.0, category: "dairy", quantity: 63 },
+    { id: 8, name: "sourdough", price: 5.5, category: "grains", quantity: 81 },
+  ];
+  
+  console.log("Welcome! We carry the following items:");
+  logNames(INVENTORY);
+  
+  console.log("Here are the names again in all uppercase:");
+  console.log(getUppercaseNames(INVENTORY));
+  
+  console.log(`In total, we have ${countItems(INVENTORY)} items in stock.`);
+  
+  const totalCost = calculateTotalPrice(INVENTORY);
+  console.log(
+    `It would cost $${totalCost?.toFixed(2)} to purchase everything in stock.`
+  );
+  
+  const itemId = prompt("Enter the ID of an item:", "1");
+  console.log(`The item with id #${itemId} is:`);
+  console.log(getItemById(INVENTORY, +itemId));
+  
+  const itemName = prompt("Enter the name of an item:", "apple");
+  console.log(
+    `The price of ${itemName} is ${getItemPriceByName(INVENTORY, itemName)}.`
+  );
+  
+  const category = prompt("Enter a category you would like to see:", "fruit");
+  console.log(`The items in the ${category} category are:`);
+  console.log(getItemsByCategory(INVENTORY, category));
